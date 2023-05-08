ea98a7b171cd3d3c511ab0a270113b7944ebc0c2
soraincloud <89382076+soraincloud@users.noreply.github.com>
2023-05-08 13:27:35 +0800

    create vue and spring project

---
+++ back_springboot/src/main/java/com/spring/back_springboot/BackSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.spring.back_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackSpringbootApplication.class, args);
+    }
+
+}
---
+++ back_springboot/src/test/java/com/spring/back_springboot/BackSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.back_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
