c37eceea748711f0b59aa325c8013630401fbb41
soraincloud <89382076+soraincloud@users.noreply.github.com>
2023-06-16 18:07:53 +0800

    add sql: user and tag

--- front_vue3/src/components/personal/PersonalIndex.vue
+++ front_vue3/src/components/personal/PersonalIndex.vue
@@ -1,7 +1,18 @@
 <template>
     <div class="PersonalIndex-avater-div-style" :style="paddingTop">
         <el-avatar :src="avatar" :size="150" fit="scale-down"></el-avatar>
-        <p class="PersonalIndex-text-style">{{ username }}</p>
+        <p class="PersonalIndex-name-style">{{ username }}</p>
+        <el-tag
+        v-for="(item,i) in tags"
+        :key="i"
+        effect="light"
+        type="danger"
+        size="small"
+        class="PersonalIndex-el-tag-style"
+        >
+        {{ item.content }}
+        </el-tag>
+        <p class="PersonalIndex-mark-style">{{ mark }}</p>
     </div> 
 </template>
 
@@ -14,13 +25,32 @@ export default
         return{
             avatar: require('@/assets/images/logo/head-main.png'),
             username: '没登陆所以没昵称哦',
-            paddingTop: "padding-top:" + (window.innerHeight * 0.2) + "px;",
+            paddingTop: "padding-top:" + ((window.innerHeight - 360) / 2) + "px;",
+            tags: 
+            [
+                {
+                    content: "neko",
+                },
+                {
+                    content: "nia",
+                },
+                {
+                    content: "miao",
+                },
+                {
+                    content: "wu nia",
+                },
+                {
+                    content: "www",
+                },
+            ],
+            mark: "因为没有登录所以没有个人介绍哦,快去登录,然后写一个吧  ･ω･ )"
         }
     },
     mounted()
     {
         window.addEventListener('resize',() =>
-            this.paddingTop = "padding-top:" + (window.innerHeight * 0.2) + "px;",
+            this.paddingTop = "padding-top:" + ((window.innerHeight - 360) / 2) + "px;",
         )
     },
 }
--- sql_mysql/sric-blog.sql
+++ sql_mysql/sric-blog.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50742
  File Encoding         : 65001
 
- Date: 13/06/2023 15:51:16
+ Date: 16/06/2023 17:53:36
 */
 
 SET NAMES utf8mb4;
@@ -28,7 +28,7 @@ CREATE TABLE `indextime`  (
   `type` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `color` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of indextime
@@ -43,16 +43,60 @@ CREATE TABLE `notes`  (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'NULL TYTLE',
   `description` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'NO DESCRIPTION',
-  `category` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'OTHERS',
+  `category` int(10) UNSIGNED NOT NULL DEFAULT 1,
   `md` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'NULL.md',
+  `visited` int(12) UNSIGNED NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of notes
 -- ----------------------------
-INSERT INTO `notes` VALUES (1, 'NULL TYTLE', 'NO DESCRIPTION', 'OTHERS', 'NULL.md');
-INSERT INTO `notes` VALUES (2, 'NULL TYTLE', 'NO DESCRIPTION', 'OTHERS', 'NULL.md');
-INSERT INTO `notes` VALUES (3, 'NULL TYTLE', 'NO DESCRIPTION', 'OTHERS', 'NULL.md');
+INSERT INTO `notes` VALUES (1, 'NULL TYTLE', 'NO DESCRIPTION', 1, 'NULL.md', 0);
+INSERT INTO `notes` VALUES (2, 'NULL TYTLE', 'NO DESCRIPTION', 1, 'NULL.md', 0);
+INSERT INTO `notes` VALUES (3, '二叉树', '这是一篇关于二叉树的笔记，非常的基础，如果你忘记了关于二叉树的内容，可以瞅瞅', 2, 'NULL.md', 0);
+INSERT INTO `notes` VALUES (4, 'NULL TYTLE', 'NO DESCRIPTION', 1, 'NULL.md', 0);
+
+-- ----------------------------
+-- Table structure for tag
+-- ----------------------------
+DROP TABLE IF EXISTS `tag`;
+CREATE TABLE `tag`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `uid` int(10) UNSIGNED NOT NULL,
+  `content` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tag
+-- ----------------------------
+INSERT INTO `tag` VALUES (1, 1, 'nia');
+INSERT INTO `tag` VALUES (2, 1, 'neko');
+INSERT INTO `tag` VALUES (3, 1, 'qwq');
+INSERT INTO `tag` VALUES (4, 1, 'awa');
+INSERT INTO `tag` VALUES (5, 2, 'homo');
+INSERT INTO `tag` VALUES (6, 2, '114514');
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `phonenumber` varchar(11) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `mark` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'NULL',
+  `status` int(10) UNSIGNED NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES (1, 'SRIC', 'soraincloud', '11451419198', '114514@qq.com', 'this is soraincloud speaking ~', 1);
+INSERT INTO `user` VALUES (2, 'homo', 'homo', NULL, NULL, 'NULL', 0);
 
 SET FOREIGN_KEY_CHECKS = 1;
