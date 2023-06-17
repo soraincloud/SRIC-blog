ec2e13d0d24d3a5c95c816c138afceed4c588c12
soraincloud <89382076+soraincloud@users.noreply.github.com>
2023-06-18 00:44:20 +0800

    change title and ico

--- front_vue3/vue.config.js
+++ front_vue3/vue.config.js
@@ -2,23 +2,34 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave:false,
-      publicPath: '/',
-      outputDir: 'dist',
-      assetsDir:"statics",
-      productionSourceMap: process.env.NODE_ENV !== 'production'? false : true,
-      /* webpack-dev-server 相关配置 */
-      devServer:
+  publicPath: '/',
+  outputDir: 'dist',
+  assetsDir:"statics",
+  productionSourceMap: process.env.NODE_ENV !== 'production'? false : true,
+  /* webpack-dev-server 相关配置 */
+  devServer:
+  {
+    /* 使用代理 */
+    proxy:
+    {
+      '/api':
       {
-        /* 使用代理 */
-        proxy:
-        {
-          '/api':
-          {
-            target: 'http://localhost:8443',  // 目标代理服务器地址
-            changeOrigin: true,                          // 允许跨域
-            pathRewrite:{"^/api":''}
-          },
-        },
-        port:8080
+        target: 'http://localhost:8443',  // 目标代理服务器地址
+        changeOrigin: true,                          // 允许跨域
+        pathRewrite:{"^/api":''}
       },
+    },
+    port:8080
+  },
+  pwa: 
+  {
+    iconPaths: 
+    {
+      favicon32: 'favicon.ico',
+      favicon16: 'favicon.ico',
+      appleTouchIcon: 'favicon.ico',
+      maskIcon: 'favicon.ico',
+      msTileImage: 'favicon.ico'
+    }
+  }
 })
