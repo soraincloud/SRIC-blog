3f5bc9d84ee8ae0c13fbe201f81c2156ea969859
soraincloud <3355905055@qq.com>
2023-12-10 01:34:05 +0800

    add router '/' redirect to '/index'

--- front_vue3/src/router/index.js
+++ front_vue3/src/router/index.js
@@ -3,6 +3,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 const routes = 
 [
   {
+    name: 'home',
+    path: '/',
+    redirect: '/index',
+  },
+  {
     name: "AppBody",
     path: '/AppBody',
     component: () => import('@/components/AppBody'),
